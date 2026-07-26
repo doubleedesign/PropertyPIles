b6b114f1e801aa3680b71304683961863b809c23
Leesa Ward <leesa@doubleedesign.com.au>
2026-07-26 19:43:18 +1000

    Refactor/Fix(NavMenu): Separate into own component and fix highlighting of current link

---
+++ PropertyPiles/Components/Elements/NavMenu.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace PropertyPiles.Components.Elements;
+	
+public partial class NavMenu : ComponentBase {
+	
+	[Inject]
+	private NavigationManager Nav { get; set; } = default!;
+	
+
+	private void Navigate(string anchor) { 
+		// Blazor doesn't natively handle #anchor links, so this triggers manually what a normal on navigate event would do
+		this.Nav.NavigateTo(anchor);
+	}
+}
