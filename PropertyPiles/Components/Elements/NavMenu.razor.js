807dd572ea985bcf6e78f4926acaa192563823e5
Leesa Ward <leesa@doubleedesign.com.au>
2026-07-27 13:09:21 +1000

    Refactor(NavMenu): Don't query DOM elements every time the scroll handler is called

--- PropertyPiles/Components/Elements/NavMenu.razor.js
+++ PropertyPiles/Components/Elements/NavMenu.razor.js
@@ -1,7 +1,51 @@
-﻿export function registerEventListeners(dotNetRef) {
+﻿export class NavMenu {
+	linkToSectionMap;
+	sections;
+	
+	constructor() {
+		this.sections = document.querySelectorAll("section");
+		const sectionIds = Array.from(this.sections).map((el) => el.getAttribute("id"));
+		const menuLinks = Array.from(document.querySelectorAll("nav a")).map(item => item.getAttribute('href').replace("#", ""));
+
+		// By design, not all sections have a matching menu link.
+		// In that case, it should use the previous one (e.g., "Maybe" goes to #shortlist, the same as "Priority")
+		this.linkToSectionMap = Array.from(this.sections).reduce((acc, section) => {
+			const sectionId = section.id;
+
+			if(!menuLinks.includes(sectionId)) {
+				const sectionIndex = sectionIds.findIndex(section => section === sectionId);
+				const previousOrFirst = sectionIndex > 0 ? sectionIds[sectionIndex - 1] : 0;
+				acc[sectionId] = previousOrFirst === 0 ? sectionIds[0] : sectionIds[previousOrFirst];
+
+				return acc;
+			}
+
+			acc[sectionId] = sectionId;
+			return acc;
+		}, {});
+	}
+	
+	getActiveSection() {
+		let closestSectionId = "";
+		let minDistanceToTop = window.innerHeight;
+		this.sections.forEach((section) => {
+			const sectionTop = section.getBoundingClientRect().top;
+			if (sectionTop >= 0 && sectionTop < minDistanceToTop) {
+				minDistanceToTop = sectionTop;
+				closestSectionId = section.getAttribute('id');
+			}
+		});
+
+		return this.linkToSectionMap[closestSectionId] || closestSectionId;
+	}
+}
+
+export function registerEventListeners(dotNetRef) {
+	const instance = new NavMenu(dotNetRef);
+	
 	const handler = (event) => {
 		console.log('scroll event');
-		dotNetRef.invokeMethodAsync('OnScroll', getActiveSection());
+		dotNetRef.invokeMethodAsync('OnScroll', instance.getActiveSection());
 	} 
 	
 	window.addEventListener('scrollend', handler);
@@ -10,72 +54,6 @@
 	return handler;
 }
 
-function getActiveSection() {
-	const sections = document.querySelectorAll("section");
-	const sectionIds = Array.from(sections).map((el) => el.getAttribute("id"));
-	const menuLinks = Array.from(document.querySelectorAll("nav a")).map(item => item.getAttribute('href').replace("#", ""));
-
-	// By design, not all sections have a matching menu link.
-	// In that case, it should use the previous one (e.g., "Maybe" goes to #shortlist, the same as "Priority")
-	// Also, the first one should go to #top to account for the sticky header
-	const linkToSectionPairs = Array.from(sections).reduce((acc, section) => {
-		const sectionId = section.id;
-		const sectionIndex = sectionIds.findIndex(section => section === sectionId);
-		
-		if(sectionIndex === 0) {
-			acc[sectionId] = "top";
-			return acc;
-		}
-		
-		if(!menuLinks.includes(sectionId)) {
-			const sectionIndex = sectionIds.findIndex(section => section === sectionId);
-			const previousOrFirst = sectionIndex > 0 ? sectionIds[sectionIndex - 1] : 0;
-			acc[sectionId] = previousOrFirst === 0 ? "top" : sectionIds[previousOrFirst];
-			
-			return acc;
-		}
-		
-		acc[sectionId] = sectionId;
-		return acc;
-	}, {});
-	
-	let closestSectionId = "";
-	let minDistanceToTop = window.innerHeight;
-	sections.forEach((section) => {
-		const sectionTop = section.getBoundingClientRect().top;
-		if (sectionTop >= 0 && sectionTop < minDistanceToTop) {
-			minDistanceToTop = sectionTop;
-			closestSectionId = section.getAttribute('id');
-		}
-	});
-	
-	return linkToSectionPairs[closestSectionId] || closestSectionId;
-}
-
-
 export function unregisterScrollListener(handler) {
 	window.removeEventListener('scrollend', handler);
-}
-
-export function activeMenuItemClass() {
-	
-	const sections = document.querySelectorAll("section");
-	const menuLinks = document.querySelectorAll(".site-header a");
-	let closestSectionId = "";
-	let minDistanceToTop = window.innerHeight;
-
-	sections.forEach((section) => {
-		const sectionTop = section.getBoundingClientRect().top;
-		if (sectionTop >= 0 && sectionTop < minDistanceToTop) {
-			minDistanceToTop = sectionTop;
-			closestSectionId = section.getAttribute('id');
-		}
-	});
-
-	menuLinks.forEach((link) => {
-		link.classList.remove('active');
-		if (link.getAttribute('href') === `#${closestSectionId}`) {
-			link.classList.add('active');
-		}
-	});
 }
