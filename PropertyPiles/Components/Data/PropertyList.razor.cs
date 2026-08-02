f404f8bd0c2cc7416edb906103cafc0f091ade41
Leesa Ward <leesa@doubleedesign.com.au>
2026-08-02 16:49:31 +1000

    Feat(PropertyList): Sort by days on market by default (newest first)

--- PropertyPiles/Components/Data/PropertyList.razor.cs
+++ PropertyPiles/Components/Data/PropertyList.razor.cs
@@ -34,11 +34,16 @@ public partial class PropertyList : ComponentBase {
 		await base.OnInitializedAsync(); 
 		await this.InjectedShortlistService.Init(this.InjectedFileService, this.InjectedListingDataService, this.InjectedInternetCoverageService);
 		this.Items = await this.InjectedShortlistService.GetList(this.ListName);
+		this.SortItemsByDaysOnMarket();
 		
 		if (this.InjectedShortlistService.GetErrorsForList(this.ListName).Count > 0) {
 			// TODO: Show a single error message somewhere
 		}
 		
 		this._isLoading = false;
+	}
+
+	protected void SortItemsByDaysOnMarket() {
+		this.Items = this.Items.OrderBy(p => p.Data?.DaysOnMarket).ToList();
 	}
 }
